bc22c7b5b57d1f554e48c4cf9b11490de3963086
mbagiev <mbagiev@align-alytics.com>
2017-07-03 10:00:28 +0100

    initial commit

---
+++ merge_csv_files.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import glob
+
+
+result = pd.DataFrame([])
+file_names = glob.glob("abc*")
+
+for i, file in enumerate(file_names):
+    print('Appending file ' + str(i + 1) + ' out of ' + str(len(file_names)))
+    df = pd.read_csv(file)
+    result = result.append(df)
+
+result.to_csv('result.csv')
